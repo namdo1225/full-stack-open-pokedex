ff79c26d902759c631884f2b35000a4b6181faa3
Nam Do <namdo1204@gmail.com>
2024-02-05 23:43:02 -0700

    Fixed tests

--- cypress/e2e/e2e.cy.jsx
+++ cypress/e2e/e2e.cy.jsx
@@ -8,7 +8,8 @@ describe('Pokedex', function() {
 
 describe('Pokedex Hidden Ability', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:9000/pokemon/bulbasaur')
+    cy.visit('http://localhost:9000')
+    cy.contains('bulbasaur').click()
     cy.contains('chlorophyll')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
