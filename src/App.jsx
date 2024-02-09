1f5360e2b6a7c19c36298c9256665aa60a618076
Nam Do <namdo1204@gmail.com>
2024-02-09 15:53:49 -0700

    Test commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
+/**
+ * @returns Main app component
+ */
 const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
